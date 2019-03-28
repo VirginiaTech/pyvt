fa9f638dfc5917d2b4eb02af99497394754b7c8d
PhilipConte <philipmconte@gmail.com>
2019-03-28 00:31:35 -0400

    0.0.7 - updated dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='py-vt',
     description='A Python API for the Virginia Tech Timetable of Classes',
-    version='0.0.6',
+    version='0.0.7',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6'
@@ -11,7 +11,7 @@ setup(
     keywords='Virgina Tech Timetable API',
     packages=find_packages(),
     install_requires=[
-        'beautifulsoup4==4.5.1',
-        'requests==2.12.4'
+        'beautifulsoup4==4.7.1',
+        'requests==2.21.0'
     ]
 )
